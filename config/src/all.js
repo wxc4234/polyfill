a281a5123921154402a20dd63890ecdf2ec6b2ec
wunci <875246904@qq.com>
2023-03-21 01:04:14 +0800

    add includes、Object.entries、Object.values

--- config/src/all.js
+++ config/src/all.js
@@ -3,10 +3,14 @@ import '../../feature/map/map';
 import '../../feature/symbol/symbol';
 import '../../feature/url/url';
 import '../../feature/object/assign';
+import '../../feature/object/entries';
+import '../../feature/object/values';
 import '../../feature/array/from';
 import '../../feature/array/prototype/fill';
 import '../../feature/array/prototype/find';
 import '../../feature/array/prototype/findIndex';
+import '../../feature/array/prototype/includes';
 import '../../feature/promise/promise';
 import '../../feature/fetch/fetch';
 import '../../feature/intersection-observer/intersection-observer';
+import '../../feature/element/closest'
---
+++ feature/element/closest.js
@@ -0,0 +1,11 @@
+if (!Element.prototype.closest) {
+    Element.prototype.closest = function (s) {
+        var el = this;
+        if (!document.documentElement.contains(el)) return null;
+        do {
+            if (el.matches(s)) return el;
+            el = el.parentElement;
+        } while (el !== null);
+        return null;
+    };
+}
